182d506a47453cc6e65d58e1bbb41261095a531b
Kennedy R. Winters <kwinters3@debx64template.twu.edu>
2021-10-21 09:18:53 -0500

    python madlib

---
+++ Labs/madlib.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+noun1 =input ("Please give me a noun!")
+noun2 =input ("Please give me another noun related to the noun!") 
+verb1 =input ("Now give me a verb please, also related, but in past tense.")
+verb2 =input ("Another verb please, again related please.")
+adject1 = input ("And lastly an adjective please, and make sure they're related.") 
+print ("Beep Boop..")
+print (" The" + (noun1) + (verb1) + "to get to" + (noun2) + "to" + (verb2) + (adject1))
