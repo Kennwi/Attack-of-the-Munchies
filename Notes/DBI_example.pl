b581613f2d1cc7c0118ea27798eebbd9905b091e
Kennedy <98730188+Kennwi@users.noreply.github.com>
2022-03-03 10:38:30 -0600

    Delete Notes directory

--- Notes/DBI_example.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-
-use warnings;
-use Cwd;
-
-my $vpsconnect = DBI->connect("DBI:mysql:foo",'student','');
--- Notes/five_array.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-@foo = ("Cheese", "Tiger", "Red", "Yellow", "Green");
-
-foreach my $item (@foo) {
-if ($item=~m/Cheese/i) { 
-print "$item\n";
-}
-}
